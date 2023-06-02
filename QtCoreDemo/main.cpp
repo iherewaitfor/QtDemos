c1c3ca7c45e321595e2546e1dc0b9c6a5e5d2b96
yangkemin <iherewaitfor@163.com>
2023-06-02 14:17:10 +0800

    add QtCoreDemo. 只引用QtCore的cmake项目。

---
+++ QtCoreDemo/main.cpp
@@ -0,0 +1,10 @@
+#include <QtCore>
+#include "numberlogic.h"
+#include <iostream>
+int main(int argc, char *argv[])
+{
+    NumberLogic numberLogic;
+    std::cout << "Hello world" << std::endl;
+    //QEventLoop eventloop;
+    //eventloop.exec();
+}
---
+++ QtCoreDemo/numberlogic.h
@@ -0,0 +1,26 @@
+#ifndef NUMBERLOGIC_H
+#define NUMBERLOGIC_H
+
+#include <QObject>
+
+class NumberLogic : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(qint32 counter READ counter WRITE setCounter)
+
+public:
+    NumberLogic(QObject*parent = nullptr);
+    qint32 counter();
+    void setCounter(qint32 counter);
+    Q_INVOKABLE void addCount(qint32 i); // can be invoked via the meta-object system
+    void nornmalAddCount(qint32 i);
+public slots:
+    void onIncreace();
+signals:
+    void counterChanged(int counter);
+    void counterChangedText(QString strCounter);
+private:
+    qint32 m_counter;
+};
+
+#endif
